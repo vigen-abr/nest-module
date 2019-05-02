c4d59dbb2df1b27a183d284038a679038cea3c9b
Vigen Abrahamyan <vigen.abrahamyan@fluxtechs.com>
2019-05-02 17:35:37 +0400

    npm configs

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export function findEmojis(str: string): string[] {
+  // add runtime check for use in JavaScript
+  if (typeof str !== 'string') {
+    return [];
+  }
+
+  return ['my-module'];
+}
